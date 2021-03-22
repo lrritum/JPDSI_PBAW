e7fc73dcf20cb8a65801a8fe5c8380a9e74d60db
lrritum <44048552+lrritum@users.noreply.github.com>
2021-03-22 23:44:24 +0100

    Add files via upload

--- php_03_proste_szablony/app/security/login_view.php
+++ php_03_proste_szablony/app/security/login_view.php
@@ -37,12 +37,12 @@ include _ROOT_PATH.'/templates/top.php';
             <input type="submit" value="Zaloguj" class="pure-button pure-button-primary"/>
         </div>
 </form>	
-
+    <div width: 100% style="align-content: center;">
 <?php
 //wyświeltenie listy błędów, jeśli istnieją
 if (isset($messages)) {
 	if (count ( $messages ) > 0) {
-		echo '<ol style="padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		echo '<ol style="padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #ad172d; width:300px;margin:auto">';
 		foreach ( $messages as $key => $msg ) {
 			echo '<li>'.$msg.'</li>';
 		}
@@ -52,10 +52,12 @@ if (isset($messages)) {
 ?>
 
 </div>
+</div>
 <?php //dół strony z szablonu 
 include _ROOT_PATH.'/templates/bottom.php';
 ?>
 </div>
+    
 			<script src="_APP_ROOT/assets/js/jquery.min.js"></script>
 			<script src="_APP_ROOT/assets/js/jquery.poptrox.min.js"></script>
 			<script src="_APP_ROOT/assets/js/skel.min.js"></script>
