c4b91f25cf5901f6c1f8e156dfde62333559c88f
lrritum <44048552+lrritum@users.noreply.github.com>
2021-03-22 22:57:38 +0100

    Add files via upload

---
+++ php_03_proste_szablony/templates/bottom.php
@@ -0,0 +1,6 @@
+                        <footer id="footer">
+						<div>
+						<p>Kosmiczny kalkulator rat na bazie gotowego szablonu
+						<p>&copy; Untitled. All rights reserved. Design: <a href="http://templated.co">TEMPLATED</a>. Demo Images: <a href="http://unsplash.com">Unsplash</a>.</p>
+						<div>
+			</footer>
